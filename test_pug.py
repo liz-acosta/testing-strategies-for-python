6c816e397f64643bc798e7de9e682984781c4cd4
liz-acosta <elizabeth.r.acosta@gmail.com>
2023-09-14 16:52:39 -0700

    edited tests

--- test_pug.py
+++ test_pug.py
@@ -9,22 +9,21 @@ class TestPug(unittest.TestCase):
         """Tests if the instatiation of a class Pug is successful"""
 
         test_result = Pug("Gary", "14", "San Francisco", "5:00 PM")
-        test_data = ({'test_result': test_result.name,
+        test_data = [{'test_result': test_result.name,
                     'expected_result': "Gary",},
                     {'test_result': test_result.age,
                     'expected_result': 14,},
                     {'test_result': test_result.home,
                     'expected_result': "San Francisco",},
                     {'test_result': test_result.puppy_dinner,
-                    'expected_result': "17:00"})
+                    'expected_result': "17:00"}]
         for data in test_data:
-            with self.subTest(msg="Valid data, successful instantiation"):
-                self.assertEqual(data['test_result'], data['expected_result'])
+            self.assertEqual(data['test_result'], data['expected_result'])
     
     def test_pug_instance_exceptions(self):
         """Tests if the instatiation of a class Pug results in the correct exception"""
         
-        test_data = ({'case': "Invalid age",
+        test_data = [{'case': "Invalid age",
                       'name': "Gary",
                       'age': "fourteen",
                       'home': "San Francisco",
@@ -39,13 +38,12 @@ class TestPug(unittest.TestCase):
                        'puppy_dinner': "5:00",
                        'expected_result': 
                        "Error creating PUG with name Gary: time data '5:00' does not match format '%I:%M %p'"
-                       })
-        
+                       }]
         for data in test_data:
             with self.subTest(msg=data['case']):
                 with self.assertRaises(Exception) as test_e:
                     Pug(data['name'], data['age'], data['home'], data['puppy_dinner'])
-                self.assertEqual(str(test_e.exception), data['expected_result'], 
+                self.assertEqual(str(test_e.exception), data['expected_result'],
                 msg="Test for pug instantiation exception failed")
 
 
@@ -67,13 +65,13 @@ class TestPugWithSetup(unittest.TestCase):
     def test_check_for_puppy_dinner(self, mock_datetime):
         """Tests the check_for_puppy_dinner function"""
         
-        test_data = ({'case': "Puppy dinner time",
+        test_data = [{'case': "Puppy dinner time",
                       'time': "17:00",
                       'expected_result': "The current time is 17:00. It is time for puppy dinner!"},
                       {'case': "Not puppy dinner time",
                       'time': "16:00",
                       'expected_result': 
-                      "The current time is 16:00. It is not yet time for puppy dinner."})        
+                      "The current time is 16:00. It is not yet time for puppy dinner."}]
         for data in test_data:
             with self.subTest(msg=data['case']):
                 mock_datetime.now().strftime.return_value = data['time']
