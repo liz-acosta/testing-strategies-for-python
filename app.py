8461a576b4e827ad6650fb006d864d2a0b809ac2
liz-acosta <elizabeth.r.acosta@gmail.com>
2024-02-10 19:06:32 -0800

    add pug facts

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms.fields import *
+
+import requests
+import json
+
 from .pug import Pug
 from .form import PugForm, FormError
+
+BASE_URL = 'https://dogapi.dog/api/v2/'
+PUG_ID = 'a6ea38ed-f692-478e-af29-378d0e2cc270'
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -44,6 +51,11 @@ def create_app(configfile=None):
         puppy_dinner_result = Pug.check_for_puppy_dinner(puppy_dinner=session['puppy_dinner'])
         session['puppy_dinner_result'] = puppy_dinner_result
         return render_template('heresyourpug.html', pug_description=session['pug_description'], pug_image=session['pug_image'], puppy_dinner_result=session['puppy_dinner_result'])
+    
+    # @app.route("/pugfacts", methods=['GET'])
+    # def pug_facts():
+    #     pug_breed_facts = json.loads(requests.get(BASE_URL + 'breeds/' + PUG_ID).content)['data']['attributes']
+    #     return render_template('pugfacts.html', pug_breed_facts=pug_breed_facts)
     
     return app
 
